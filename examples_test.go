d29bdd2674e4c3260e787920ab4180b0d10c048a
zakuro <z@kuro.red>
2021-01-13 17:16:36 +0900

    Improve exmaples_test

--- examples_test.go
+++ examples_test.go
@@ -6,13 +6,19 @@ import (
 
 func ExampleHello() {
 	os.Setenv("ZCONFIG", "examples/hello.yaml")
+	realMain([]string{})
 	realMain([]string{"hello"})
 	realMain([]string{"hello", "world"})
+	realMain([]string{"hello.world"})
 	realMain([]string{"hello", "script"})
 	realMain([]string{"hello", "python"})
 	// Output:
 	// hello you
 	// bye you
+	// hello you
+	// bye you
+	// hello world
+	// bye world
 	// hello world
 	// bye world
 	// hello script
