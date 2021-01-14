69dc818cdfb926b18d9be9cdd578426a02873ae3
zakuro <z@kuro.red>
2021-01-14 09:06:07 +0900

    Update example

--- examples_test.go
+++ examples_test.go
@@ -24,12 +24,3 @@ func ExampleHello() {
 	// hello script
 	// hello python
 }
-
-func ExampleEcho() {
-	os.Setenv("ZCONFIG", "examples/hello.yaml")
-	realMain([]string{"echo.hey"})
-	realMain([]string{"echo", "hey"})
-	// Output:
-	// hey
-	// hey
-}
