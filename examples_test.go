cc41cd074b880c50ad2b425fa4e4e8bfd7e65e57
zakuro <z@kuro.red>
2021-01-14 18:37:35 +0900

    Update example

--- examples_test.go
+++ examples_test.go
@@ -37,10 +37,17 @@ func ExampleEcho() {
 
 func ExampleEnv() {
 	os.Setenv("ZCONFIG", "examples/hello.yaml")
+	os.Unsetenv("MESSAGE")
 	realMain([]string{"echo.env.message"})
 	os.Setenv("MESSAGE", "system")
 	realMain([]string{"echo.env.message"})
+	os.Unsetenv("MESSAGE")
+	realMain([]string{"echo.env.message2"})
+	os.Setenv("MESSAGE", "system")
+	realMain([]string{"echo.env.message2"})
 	// Output:
 	// message
+	// system
+	// message2
 	// system
 }
