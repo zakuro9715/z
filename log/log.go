8e7c00cba61827cbe746f2f014fff03bfd660390
zakuro <z@kuro.red>
2021-01-21 14:43:48 +0900

    Exit when fatal log

--- log/log.go
+++ log/log.go
@@ -49,20 +49,22 @@ func Logf(level Level, format string, a ...interface{}) (int, error) {
 	return Default.Logf(level, format, a...)
 }
 
-func (l *Logger) Fatal(s string) (int, error) {
-	return l.Log(FATAL, s)
+func (l *Logger) Fatal(s string) {
+	l.Log(FATAL, s)
+	os.Exit(2)
 }
 
-func Fatal(s string) (int, error) {
-	return Default.Fatal(s)
+func Fatal(s string) {
+	Default.Fatal(s)
 }
 
-func (l *Logger) Fatalf(s string, a ...interface{}) (int, error) {
-	return l.Logf(FATAL, s, a...)
+func (l *Logger) Fatalf(s string, a ...interface{}) {
+	l.Logf(FATAL, s, a...)
+	os.Exit(2)
 }
 
-func Fatalf(s string, a ...interface{}) (int, error) {
-	return Default.Fatalf(s, a...)
+func Fatalf(s string, a ...interface{}) {
+	Default.Fatalf(s, a...)
 }
 
 func (l *Logger) Info(s string) (int, error) {
