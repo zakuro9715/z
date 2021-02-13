f9ca99ac5ce8fbf25e6ca46eee04ef4532258d6c
zakuro <z@kuro.red>
2021-02-13 12:06:40 +0900

    clean

--- yaml/yaml.go
+++ yaml/yaml.go
@@ -2,27 +2,28 @@ package yaml
 
 import (
 	"github.com/goccy/go-yaml"
+	goyaml "github.com/goccy/go-yaml"
 )
 
 var (
-	decodeOptions = []yaml.DecodeOption{yaml.DisallowDuplicateKey()}
+	decodeOptions = []goyaml.DecodeOption{yaml.DisallowDuplicateKey()}
 )
 
 type StringList []string
 
 func (v *StringList) UnmarshalYAML(data []byte) error {
 	var str string
-	if err := yaml.Unmarshal(data, &str); err == nil {
+	if err := Unmarshal(data, &str); err == nil {
 		*v = []string{str}
 		return nil
 	}
 
 	ss := []string{}
-	err := yaml.Unmarshal(data, &ss)
+	err := Unmarshal(data, &ss)
 	*v = ss
 	return err
 }
 
 func Unmarshal(data []byte, v interface{}) error {
-	return yaml.UnmarshalWithOptions(data, v, decodeOptions...)
+	return goyaml.UnmarshalWithOptions(data, v, decodeOptions...)
 }
